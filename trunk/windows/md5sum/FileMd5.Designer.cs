2b40eeb0c6c49857e22f2dc785a272bef3269259
linxh.china@gmail.com <linxh.china@gmail.com@53787947-dd4f-0410-b885-6f950aad2415>
2009-10-17 04:02:11 +0000

    md5sum

---
+++ trunk/windows/md5sum/FileMd5.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace md5sum
+{
+    partial class FileMd5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Exit = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SelectFile = new System.Windows.Forms.Button();
+            this.File = new System.Windows.Forms.Label();
+            this.md5 = new System.Windows.Forms.Label();
+            this.textFileName = new System.Windows.Forms.TextBox();
+            this.textMd5sum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(160, 69);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(52, 24);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // SelectFile
+            // 
+            this.SelectFile.Location = new System.Drawing.Point(72, 69);
+            this.SelectFile.Name = "SelectFile";
+            this.SelectFile.Size = new System.Drawing.Size(50, 24);
+            this.SelectFile.TabIndex = 1;
+            this.SelectFile.Text = "SelectFile";
+            this.SelectFile.UseVisualStyleBackColor = true;
+            this.SelectFile.Click += new System.EventHandler(this.SelectFile_Click);
+            // 
+            // File
+            // 
+            this.File.AutoSize = true;
+            this.File.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.File.Location = new System.Drawing.Point(6, 10);
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(34, 20);
+            this.File.TabIndex = 2;
+            this.File.Text = "File";
+            // 
+            // md5
+            // 
+            this.md5.AutoSize = true;
+            this.md5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.md5.Location = new System.Drawing.Point(6, 41);
+            this.md5.Name = "md5";
+            this.md5.Size = new System.Drawing.Size(40, 20);
+            this.md5.TabIndex = 3;
+            this.md5.Text = "md5";
+            // 
+            // textFileName
+            // 
+            this.textFileName.Location = new System.Drawing.Point(55, 12);
+            this.textFileName.Name = "textFileName";
+            this.textFileName.ReadOnly = true;
+            this.textFileName.Size = new System.Drawing.Size(210, 20);
+            this.textFileName.TabIndex = 4;
+            // 
+            // textMd5sum
+            // 
+            this.textMd5sum.Location = new System.Drawing.Point(52, 43);
+            this.textMd5sum.Name = "textMd5sum";
+            this.textMd5sum.ReadOnly = true;
+            this.textMd5sum.Size = new System.Drawing.Size(213, 20);
+            this.textMd5sum.TabIndex = 5;
+            // 
+            // FileMd5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 99);
+            this.Controls.Add(this.textMd5sum);
+            this.Controls.Add(this.textFileName);
+            this.Controls.Add(this.md5);
+            this.Controls.Add(this.File);
+            this.Controls.Add(this.SelectFile);
+            this.Controls.Add(this.Exit);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FileMd5";
+            this.Text = "File md5sum";
+            this.Load += new System.EventHandler(this.FileMd5_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button SelectFile;
+        private System.Windows.Forms.Label File;
+        private System.Windows.Forms.Label md5;
+        private System.Windows.Forms.TextBox textFileName;
+        private System.Windows.Forms.TextBox textMd5sum;
+    }
+}
+
---
+++ trunk/windows/md5sum/FileMd5.cs
@@ -0,0 +1,107 @@
+﻿/**
+Copyright 2009 http://code.google.com/p/toolkits/. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following
+    disclaimer in the documentation and/or other materials provided
+    with the distribution.
+  * Neither the name of http://code.google.com/p/toolkits/ nor the names of its
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace md5sum
+{
+    public partial class FileMd5 : Form
+    {
+        private string filename;
+        private string md5sum;
+
+        public FileMd5()
+        {
+            InitializeComponent();
+            this.filename = "";
+            md5sum = "";
+            this.openFileDialog = new OpenFileDialog();
+        }
+
+        private void FileMd5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SelectFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            this.filename = this.openFileDialog.FileName;
+            textFileName.Text = this.filename;
+            textMd5sum.Text = "Calculating";
+            Thread th = new Thread(new ThreadStart(this.Md5sum));
+            th.Start();
+            th.Join();
+            try
+            {
+                textMd5sum.Text = this.md5sum;
+            }
+            catch (Exception MSG)
+            {
+                MessageBox.Show(MSG.ToString());
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Md5sum()
+        {
+            try
+            {
+                FileStream fs = System.IO.File.OpenRead(this.filename);
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] result = md5.ComputeHash(fs);
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < result.Length; i++)
+                {
+                    sBuilder.Append(result[i].ToString("X2"));
+                }
+                this.md5sum = sBuilder.ToString();
+                //this.md5sum = BitConverter.ToString(result);
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.ToString());
+            }
+        }
+    }
+}

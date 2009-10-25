6b7d86e6044ab432a2714120d5042af61d8067cd
linxh.china <linxh.china@53787947-dd4f-0410-b885-6f950aad2415>
2009-10-25 13:42:54 +0000

    update

--- windows/HiBaiduAlbumDownloader/AlbumDownloader.Designer.cs
+++ windows/HiBaiduAlbumDownloader/AlbumDownloader.Designer.cs
@@ -28,21 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AlbumDownloader));
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.labelDestDir = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.buttonSelectDir = new System.Windows.Forms.Button();
+            this.textBoxDestDir = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
+            // 
+            // labelUserName
+            // 
+            resources.ApplyResources(this.labelUserName, "labelUserName");
+            this.labelUserName.BackColor = System.Drawing.SystemColors.Control;
+            this.labelUserName.Name = "labelUserName";
+            // 
+            // labelDestDir
+            // 
+            resources.ApplyResources(this.labelDestDir, "labelDestDir");
+            this.labelDestDir.Name = "labelDestDir";
+            // 
+            // buttonOK
+            // 
+            resources.ApplyResources(this.buttonOK, "buttonOK");
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCancel
+            // 
+            resources.ApplyResources(this.buttonCancel, "buttonCancel");
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // textBoxUserName
+            // 
+            resources.ApplyResources(this.textBoxUserName, "textBoxUserName");
+            this.textBoxUserName.Name = "textBoxUserName";
+            // 
+            // buttonSelectDir
+            // 
+            resources.ApplyResources(this.buttonSelectDir, "buttonSelectDir");
+            this.buttonSelectDir.Name = "buttonSelectDir";
+            this.buttonSelectDir.UseVisualStyleBackColor = true;
+            this.buttonSelectDir.Click += new System.EventHandler(this.buttonSelectDir_Click);
+            // 
+            // textBoxDestDir
+            // 
+            resources.ApplyResources(this.textBoxDestDir, "textBoxDestDir");
+            this.textBoxDestDir.Name = "textBoxDestDir";
+            this.textBoxDestDir.ReadOnly = true;
             // 
             // AlbumDownloader
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 262);
+            this.Controls.Add(this.textBoxDestDir);
+            this.Controls.Add(this.buttonSelectDir);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.labelDestDir);
+            this.Controls.Add(this.labelUserName);
             this.Name = "AlbumDownloader";
-            this.Text = "AlbumDownloader";
             this.Load += new System.EventHandler(this.AlbumDownloader_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.Label labelDestDir;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.Button buttonSelectDir;
+        private System.Windows.Forms.TextBox textBoxDestDir;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
     }
 }
 

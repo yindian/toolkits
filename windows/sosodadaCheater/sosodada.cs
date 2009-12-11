4c8a198d99a723b8af84618c8160fee85a9116f8
linxh.china <linxh.china@gmail.com>
2009-12-11 12:46:38 +0000

    sosodada

---
+++ windows/sosodadaCheater/sosodada.cs
@@ -0,0 +1,92 @@
+﻿/**
+Copyright 2009 http://code.google.com/p/toolkits/. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following
+    disclaimer in the documentation and/or other materials provided
+    with the distribution.
+  * Neither the name of http://code.google.com/p/toolkits/ nor the names of its
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace sosodadaCheater
+{
+    public partial class sosodadaForm : Form
+    {
+        public sosodadaForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+           
+            MethodInvoker mi = new MethodInvoker(this.Login);
+            mi.BeginInvoke(null, null);
+           
+        }
+
+        private void Login()
+        {
+            for(int i=0;i<10;i++)
+            {
+                MockLogin logger = new MockLogin(this.email.Text, this.password.Text);
+                logger.onInfoUpdate += new MockLogin.updateInfo(UpdateInfo);
+                string content = null;
+                content = logger.Login();
+                content = logger.SkipQuestions(content);
+                content = logger.GetScore(content);
+                logger.ShowScore(i+"", content);
+                Thread.Sleep(3000);
+            }
+        }
+
+        private void UpdateInfo(string curr, string info)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MockLogin.updateInfo(UpdateInfo), new object[] { curr, info });
+            }
+            else
+            {
+                this.info.Text = curr+", "+info;
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
